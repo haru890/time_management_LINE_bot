ff37ff248c3fadc2af3d85eebb1e30d66b544c99
haru90 <nakamigawa.h@gmail.com>
2020-08-03 21:36:21 +0900

    リファクタリング

--- time_management.js
+++ time_management.js
@@ -1,60 +1,51 @@
 // スプレッドシートに情報を記載しておき,そこから読み込む。
 const UPDATE_INFO_SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('更新情報');
 const SYSTEM_INFO_SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('システム情報');
-const LINE_URL = SYSTEM_INFO_SHEET.getRange(1, 1, 1, 1).getValues();// スプレッドシートから値を取得
-const LINE_TOKEN = SYSTEM_INFO_SHEET.getRange(2, 1, 1, 1).getValues();// スプレッドシートから値を取得
+const LINE_URL = SYSTEM_INFO_SHEET.getRange(1, 1, 1, 1).getValues();
+const LINE_TOKEN = SYSTEM_INFO_SHEET.getRange(2, 1, 1, 1).getValues();
 
 function doPost(e) {
   // 入力処理
-  const json = JSON.parse(e.postData.contents);// e:イベントオブジェクトのe　json形式を扱えるように変換 .でつないだ書き方ができる
+  const json = JSON.parse(e.postData.contents);// e:イベントオブジェクト　json形式を扱えるように変換 .でつないだ書き方が可能
   const replyToken = json.events[0].replyToken;// replyToken メッセージに対する返信に使う
   const userMessage = json.events[0].message.text;
   
   // 半角スペースでスプリット
   const cmd = userMessage.split(' ')[0];// 残り時間 or 更新 or 確認
-  const arg1 = userMessage.split(' ')[1];// 更新内容　→更新（分）
-  const arg2 = userMessage.split(' ')[2];// 時間　→備考空白も許容したい
-  // 予定はなくす
-  //let schedule = UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getLastRow(), 6, 1, 1).getValues();
+  const arg1 = userMessage.split(' ')[1];// 更新（分）
+  const arg2 = userMessage.split(' ')[2];// 備考（空文字を許容）
   
-  // メイン処理
-  let replyMessage;// 返信内容は変わるため、let
+  // 出力処理
+  let replyMessage;// 返信メッセージ
   //　残り（分）
   let remainingTime = Number(UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getLastRow(), 4, 1, 1).getValues());
   switch (cmd) {
     case '残り時間':
-      replyMessage = [`残り時間は${remainingTime}分です`];
+      replyMessage = [`残り時間は${remainingTime}分だよ！`];
       break;
       
     case '更新':
-      //const updateDate = getUpdateDate();// 更新日時
       UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getLastRow() + 1, 2, 1, 1).setValue(getUpdateDate());// 日付
       UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getLastRow(), 3, 1, 1).setValue(arg1);// 更新（分）
-      //UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getRange(getLastRow(), 1, 1, 1).getLastRow(), 3, 1, 1).setValue(arg1);// 更新（分）
       let updateTime = Number(arg1);
-      let lastRemainingTime = remainingTime + updateTime; // constでいいかも？
+      let lastRemainingTime = remainingTime + updateTime;
       if (updateTime > 0) {
         updateTime = `+${updateTime}`;
       }
       UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getLastRow(), 4, 1, 1).setValue(lastRemainingTime);// 残り（分）
       UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getLastRow(), 5, 1, 1).setValue(arg2);// 備考
-      replyMessage = [`更新 ${updateTime}(分) されました\n残り ${lastRemainingTime}(分) です`];
+      replyMessage = [`更新 ${updateTime}(分)\n残り   ${lastRemainingTime}(分) になったよ！`];
       break;
     
     case 'リンク':
-      replyMessage = ['https://docs.google.com/spreadsheets/d/1bnTEdDi9M-hj-WLQaTd7iaQ7OdFgjBWdH09pJ0TvzWQ/edit#gid=0'];
+      replyMessage = ['詳細はリンクをクリックしてみてね！\nhttps://docs.google.com/spreadsheets/d/1bnTEdDi9M-hj-WLQaTd7iaQ7OdFgjBWdH09pJ0TvzWQ/edit#gid=0'];
       break;
       
     case '承認':
-        // 最終行に「✔」を記入する
-      //UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getLastRow(), 1, 1, 1)
-      
+      // 「承認行」の最終行に「✔」を記入する
       const lastRowOfColumnA = UPDATE_INFO_SHEET.getRange(1, 1).getNextDataCell(SpreadsheetApp.Direction.DOWN).getRow();
       UPDATE_INFO_SHEET.getRange(lastRowOfColumnA + 1, 1).setValue('✔');
-      
-      //UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getRange(UPDATE_INFO_SHEET.getLastRow(), 1, 1, 1)
-    //                         UPDATE_INFO_SHEET.getLastRow(), 1, 1, 1).setValue('✔');
-      replyMessage = ['ご確認いただき、ありがとうございました！'];
+      replyMessage = ['承認ありがとう！'];
       break;
       
     default:
@@ -70,7 +61,7 @@ function reply(replyMessage, replyToken) {
   const url = LINE_URL;
   const headers = {
     'Content-Type': 'application/json; charset=UTF-8',// リファレンスに記載。UTF-8：文字化け防止
-    'Authorization': 'Bearer ' + LINE_TOKEN// 
+    'Authorization': 'Bearer ' + LINE_TOKEN
   };
   const data = {
     'replyToken': replyToken,
